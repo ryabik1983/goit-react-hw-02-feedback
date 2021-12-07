4609147a2291fb1f84fe9964761cb3206f66443c
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2021-12-08 00:21:45 +0200

    hw2
    hw2

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,21 @@
+// class Feedback extends React.Component {
+
+//     constructor () {
+//         super ();
+//         this.state = {
+//             good: 0,
+//             neutral: 0,
+//             bad: 0
+//         }
+
+//     }
+//     countTotalFeedback = () => {};
+//     countPositiveFeedbackPercentage = () => {};
+
+//     render() {
+//         return (
+
+//         )
+//     }
+
+// }
