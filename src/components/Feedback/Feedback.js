345c2497ef5c961203f6f32d0d46227d4ba2a3f7
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2021-12-08 23:21:46 +0200

    hw2
    hw2

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,5 +1,10 @@
-// class Feedback extends React.Component {
+// export class Feedback extends React.Component {
+//     static defaultProps ={
+//         initialValue: 0;
+//     };
+//     static Ppoptypes = {
 
+//     };
 //     constructor () {
 //         super ();
 //         this.state = {
@@ -9,11 +14,25 @@
 //         }
 
 //     }
-//     countTotalFeedback = () => {};
+//     countTotalFeedback = () => {
+//     this.setState ((prevState) => {
+// return {
+//     good: prevState.good + 1,
+// }
+//     });
+// };
+
+// };
 //     countPositiveFeedbackPercentage = () => {};
 
 //     render() {
 //         return (
+//             <div className = "Feedback">
+//                 <h1 className ="Feedback__title">Plaese leave feedback</h1>
+//                 <button type="button" className="Feedback__button" onClick = {this.toggle}>Good</button>
+//                 <button type="button" className="Feedback__button" onClick = {this.toggle}>Neutral</button>
+//                 <button type="button" className="Feedback__button" onClick = {this.toggle}>Bad</button>
+//             </div>
 
 //         )
 //     }
